6a1fcc9b74137cc275c5791656673df24fa619e7
dglalperen <alperen.dagli43@gmail.com>
2023-03-31 15:11:17 +0200

    Add project files.

---
+++ Nachhilfe/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace DiceRollRaceTo100
+{
+    class Program
+    {
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            int playerScore = 0;
+            int computerScore = 0;
+            int targetScore = 100;
+
+            Console.WriteLine("Willkommen!");
+            Console.WriteLine("Spielregeln:");
+            Console.WriteLine("Der erste der 100 Punkte sammelt gewinnt! Würfelst du eine 1, ist dein Gegner dran.");
+
+
+            while (playerScore < targetScore && computerScore < targetScore)
+            {
+                Console.WriteLine($"\nAktueller Punktestand - Spieler: {playerScore}, Computer: {computerScore}");
+                Console.WriteLine($"-----------------------------------");
+
+                playerScore += PlayerTurn();
+                if (playerScore >= targetScore)
+                {
+                    Console.WriteLine($"Glückwunsch! Du hast mit {playerScore} das Spiel gewonnen!");
+                    break;
+                }
+
+                computerScore += ComputerTurn();
+                if (computerScore >= targetScore)
+                {
+                    Console.WriteLine($"Der Computer hat mit {computerScore} Punkten gewonnen!");
+                }
+            }
+
+            Console.WriteLine("Danke fürs Spielen!");
+        }
+
+        static int PlayerTurn()
+        {
+            int turnScore = 0;
+            int rollCount = 0;
+
+            while (rollCount < 5 && turnScore < 20)
+            {
+                int roll = RollDice();
+                Console.WriteLine($"Du hast eine {roll} gewürfelt");
+                rollCount++;
+
+                if (roll == 1)
+                {
+                    Console.WriteLine("Du hast alle deine Punkte verloren!");
+                    Console.WriteLine($"-----------------------------------");
+
+                    return 0;
+                }
+
+                turnScore += roll;
+
+                if (turnScore >= 20)
+                {
+                    Console.WriteLine($"Du hast {turnScore} Punkte in dieser Runde gemacht. Die Runde ist vorbei.");
+                    break;
+                }
+
+                if (rollCount < 5)
+                {
+                    Console.WriteLine($"Dein Aktueller Punktestand ist {turnScore}. Möchtest du nochmal würfeln (y/n)");
+                    string response = Console.ReadLine().ToLower();
+                    if (response != "y" && response != "yes")
+                    {
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"-----------------------------------");
+            return turnScore;
+        }
+
+        static int ComputerTurn()
+        {
+            int turnScore = 0;
+
+            for (int i = 0; i < 2; i++)
+            {
+                int roll = RollDice();
+                Console.WriteLine($"Der Computer hat eine {roll} gewürfelt");
+
+                if (roll == 1)
+                {
+                    Console.WriteLine("Der Computer hat alle seine Punkte verloren!");
+                    return 0;
+                }
+
+                turnScore += roll;
+            }
+
+            Console.WriteLine($"Der Computer hat einen Punktestand von {turnScore}");
+            Console.WriteLine($"-----------------------------------");
+
+            return turnScore;
+        }
+
+        static int RollDice()
+        {
+            return random.Next(1, 7);
+        }
+    }
+}
---
+++ Nachhilfe/Program3.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleUI
+{
+    class Programm
+    {
+        static void Main2(string[] args)
+        {
+            const int MAX_POINTS = 100;
+            int gamerDiceCount = 0; // Aktuelle Gesamtpunktzahl
+            int refereeDiceCount = 0;
+
+            Console.WriteLine("---Spielstart---");
+            Console.WriteLine("Hallo Spieler oder Spielerin");
+
+            Console.WriteLine("Du beginnst...");
+            gamerDiceCount = PlayOneSet();
+            Console.WriteLine($"Der Spieler hat {gamerDiceCount} Punkte");
+        }
+
+        public static int GetRandomDice()
+        {
+            Random randomNumberGenerator = new Random();
+
+            return randomNumberGenerator.Next(1, 6);
+        }
+
+        public static int PlayOneSet()
+        {
+            int rolledPoints = 0; // Anzahl der gesammelten Punkte
+            int rolledDices = 0; // Anzahl der gewürfelten Würfel
+            int rolledDice; // Aktuell gewürfelte Zahl
+            string gamerInput = "";
+            bool roundOver = false;
+
+            // Würfeln
+            rolledDice = GetRandomDice();
+
+            while (!roundOver && rolledDice > 1 && rolledDices < 6 && rolledPoints < 20)
+            {
+                Console.WriteLine($"Der Spieler würfelte eine {rolledDice}");
+                Console.WriteLine("\n");
+                Console.WriteLine("Möchtest Sie weiterwürfeln?");
+                Console.WriteLine("Geben Sie ja oder nein ein!");
+                Console.WriteLine("\n");
+
+                gamerInput = Console.ReadLine();
+                switch (gamerInput)
+                {
+                    case "ja":
+                        rolledPoints += rolledDice;
+                        rolledDice = GetRandomDice();
+                        rolledDices += 1;
+                        Console.WriteLine($"Aktueller Stand: {rolledPoints}");
+                        Console.WriteLine($"\n");
+
+                        break;
+                    case "nein":
+                        roundOver = true;
+                        return rolledPoints;
+                    default:
+                        Console.WriteLine("Geben Sie ja oder nein ein!");
+                        break;
+                }
+            }
+
+            Console.WriteLine("Round over, 0 Punkte");
+            return 0;
+        }
+    }
+}
